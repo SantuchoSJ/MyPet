5fdb1e704da517dd54faf419d625fcd7bdf015b5
Santucho <santiagojoaquinsantucho@gmail.com>
2021-07-03 15:33:19 -0300

    functional header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import s from "./Header.module.css"
+import {useHistory} from "react-router-dom"
 
 export default  ()=>{
+    const history = useHistory()
+    const goHome = () =>{
+        history.push("/posts")
+    }
 
 return(
     <>
     <div className={s.container}>
-        <div className={s.text}>MyPet</div>
+        <div onClick={()=> goHome()}className={s.text}>MyPet</div>
         <div className={s.text}>Welcome Lilab!</div>
     </div>
 
